85dac356b80b6483cda316b560605f9f50cf5a05
Riad Gozim <rgozim1@gmail.com>
2019-04-24 16:49:24 +0100

    Using symlinks in buildSrc folder

--- buildSrc/src/main/kotlin/org/openmicroscopy/PluginProjectPlugin.kt
+++ buildSrc/src/main/kotlin/org/openmicroscopy/PluginProjectPlugin.kt
@@ -1,12 +1,1 @@
-package org.openmicroscopy
-
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.apply
-
-class PluginProjectPlugin : Plugin<Project> {
-    override fun apply(project: Project): Unit = project.run {
-        apply<AdditionalArtifactsPlugin>()
-        apply<PluginPublishingPlugin>()
-    }
-}
+../../../../../../src/main/kotlin/org/openmicroscopy/PluginProjectPlugin.kt
